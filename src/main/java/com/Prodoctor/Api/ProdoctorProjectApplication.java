a1da9355d2369d5c574342f770a17b00b69f29f1
unknown <you@example.comkn.noronha04@gmail.com>
2020-03-21 14:08:26 -0300

    Versão inicial do projeto.

---
+++ src/main/java/com/Prodoctor/Api/ProdoctorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Prodoctor.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdoctorProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdoctorProjectApplication.class, args);
+	}
+
+}
