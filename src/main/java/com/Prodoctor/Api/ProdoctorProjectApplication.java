fe9e4bbd270a166b3e4e64f45108cbc89011a38e
unknown <you@example.comkn.noronha04@gmail.com>
2020-03-21 14:32:46 -0300

    Alteração no arquivo principal da aplicação

--- src/main/java/com/Prodoctor/Api/ProdoctorProjectApplication.java
+++ src/main/java/com/Prodoctor/Api/ProdoctorProjectApplication.java
@@ -8,6 +8,7 @@ public class ProdoctorProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProdoctorProjectApplication.class, args);
+		System.out.println("Meu primeiro Projeto");
 	}
 
 }
